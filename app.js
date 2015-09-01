3cf3d2f6f957b870e80f7aa7132d5991b058173e
Sanaumair00 <sana.umair00@gmai.com>
2015-09-01 07:23:30 +0100

    Update app.js
    created App.initApp global function inside anonymous funtion to make few variable and fuction local,local function can also use loca variable
    implement search button and get reponse from api
    created get url closure function

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
 
 	
-	console.log("hello");	
+	App.initApp();	
 
 });
 
@@ -17,4 +17,87 @@ var App ={
 	flag: 0
 	
 };
+
+
+(function(){
+	
+	//var urlSubstr1 ="https://api.foursquare.com/v2/venues/";
+	//var urlSubstr2= 
+	
+	var getUrl = function(urlSubstr1){
+		
+		
+		return function(urlSubstr2,query,nearplace){
+			var moreParam ="";
+			if (nearplace !=""){
+				moreParam="&near="+nearplace;
+			}else{
+				moreParam="&ll="+App.locationLatLong;		
+			}
+			return urlSubstr1+urlSubstr2+"?client_id="+App.clientData.id+"&client_secret="+App.clientData.secret+"&v="+App.version+"&query="+query+moreParam;
+			
+		};
+	}
+	
+
+	//alert(getUrl("https://api.foursquare.com/v2/venues/"));
+	App.initApp =function(){	
+		
+		var generateUrl = getUrl("https://api.foursquare.com/v2/venues/");
+		$("#main-search-button").click(function(event){
+			event.preventDefault();
+			var query1= $("input#srchXplore").val();
+				var nearplace1 =$("input#near").val();
+				
+				var generateUrl = getUrl("https://api.foursquare.com/v2/venues/");
+
+				var getSearchUrl =generateUrl("search",query1,nearplace1);
+				console.log(getSearchUrl);
+				var str ="";
+				//
+				
+				$("ul#list").remove();
+				
+				$.ajax({
+					url: getSearchUrl,
+					
+					//url:"errorURl",
+					success: function(result){
+						setTimeout(function(){
+								$("#loading").hide();
+						},1000);
+						$.each( result.response.venues, function( i, value ) {
+							
+							if(value.url!= undefined){
+								str+="<li><a href='"+value.url+"' target='_blank'>"+ value.name+"</a></li>";
+							}else{
+								str+="<li>"+ value.name+"</li>";
+							}
+							
+						});
+
+						$("nav#results").html("<ul id='list'>"+str+"</ul>");
+					},
+					error: function(e){
+						alert("error");
+					}
+				});
+		
+	
+			
+			//
+		});
+
+	
+	}
+	
+	
+
+
+		//alert(urlSubstr1);
+	
+	
+	
+}());
+
 
