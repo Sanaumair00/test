bc57058b44906c941a2824525a4b359f954bbfa8
Sanaumair00 <sana.umair00@gmai.com>
2015-09-01 06:20:44 +0100

    Create app.js

---
+++ app.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+
+	
+	console.log("hello");	
+
+});
+
+var App ={
+	
+	clientData : {
+		
+		id: "0OAGS4B3SODYQM2K23SN3X0UBBPJWP4PO0ATCWH4LMMADM1N",
+		secret:"YMNU2MYPGGBTJZIEXASW01GP5K2YTWIJBXAWYCI2RQVDPALL"
+	},
+	version : "20150825",
+	locationLatLong : "",
+	flag: 0
+	
+};
+
